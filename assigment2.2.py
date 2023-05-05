8ce0ec23f9dd4bd6517550b7b4f4ae8519e09577
Aliyeh-lal-alizadeh <132198359+Aliyeh-lal-alizadeh@users.noreply.github.com>
2023-05-05 15:14:31 +0330

    Add files via upload

---
+++ assigment2.2.py
@@ -0,0 +1,10 @@
+import random
+number = random.randint(1,6)
+if number ==6 :
+    print("Congratulation! you won the prize. your number is: 6 " )
+    number2= random. randint(1,6)
+    number3= random. randint(1,6)
+    print ("your second number is", number2)
+    print ("your third number is", number3)
+else:
+    print(" your number is:", number)
